1e37cbc743e5b4344a0e88c96f326f9049968b51
Stephen Malchevskii <Usamstudent21@gmail.com>
2024-06-09 17:13:08 +0300

    feat server: update commands from drafts, pt.1

--- msg/server/__init__.py
+++ msg/server/__init__.py
@@ -7,7 +7,8 @@ from common import *
 
 def parse_create_chat(args: list):
     try:
-        assert args[0] is str
+        assert isinstance(args[0], str)
+        print('0 done')
 
         result = {
             'name': args[0],
@@ -20,22 +21,26 @@ def parse_create_chat(args: list):
         for i in range(1, len(args)):
             match args[i]:
                 case '-l' | '--limit':
-                    assert type(args[i + 1]) in [int, None]
+                    assert args[i + 1].isdigit()
+                    print('1 done')
                     result['limit'] = args[i + 1]
                 case '-s' | '--security_mode':
-                    assert args[i + 1] is bool
-                    result['security_mode'] = args[i + 1]
+                    print('2 done')
+                    result['security_mode'] = True
                 case '-p' | '--password':
+                    print('3 done')
                     result['security_mode'] = True
                     result['password'] = args[i + 1]
                 case '-a' | '--autoright':
-                    assert args[i + 1] in ['Administrator', 'Editor', 'Reader', 0, 1, 2]
-                    result['autoright'] = Rights(args[i + 1])
+                    assert args[i + 1] in ['Administrator', 'Editor', 'Reader', '0', '1', '2']
+                    print('4 done')
+                    result['autoright'] = arg2Rights(args[i + 1])
                 case _:
                     continue
     except Exception:
         return {}
 
+    print("LOG:", result)
     return result
 
 
@@ -44,6 +49,7 @@ async def handler(reader, writer):
     client = writer.get_extra_info("peername")
     print(f'New Client on {client}')
     my_user = User(user_id=hash(client), username='_')
+    TM_users[my_user._user_id] = my_user
 
     send = asyncio.create_task(reader.readline())
     receive = asyncio.create_task(my_user.queue.get())
@@ -69,7 +75,7 @@ async def handler(reader, writer):
                     case 'entrance':
                         args = shlex.join(args)
                         my_user.username = args
-                        answer = f"Welcome to Terminal Messenger, {my_user.username}"
+                        answer = f"Welcome to Terminal Messenger, {my_user.username}\n"
 
                     case 'show_chatlist':
                         Avaliable = []
@@ -97,44 +103,80 @@ async def handler(reader, writer):
                             )
                         )
 
-                        answer = '\n'.join(Avaliable + Hidden)
+                        answer = '\n'.join(Avaliable + Hidden) + '\n'
 
                     case 'open_chat':
                         name, *password = args
                         chat: Chat = TM_chats[name]
 
                         if chat.security_mode:
-                            access = chat.check_password(password_to_check=password)
+                            access = chat.check_password(
+                                password_to_check=password[0] if password else None
+                            )
 
                         if not access:
-                            answer = "No access to chat"
+                            answer = "No access to chat\n"
                         else:
                             my_user.open_chat(name)
-
-                        # TODO: message to chat about your join
-
-                        answer = '{0:_^30}'.format(name.upper())
+                            # TODO: message to chat about your join
+                            answer = '{0:_^30}\n'.format(name.upper())
 
                     case 'create_chat':
                         chat_statistics = parse_create_chat(args)
-                        my_user.create_chat(
-                            name=chat_statistics["name"],
-                            limit=chat_statistics["limit"],
-                            security_mode=chat_statistics["security_mode"],
-                            password=chat_statistics["password"],
-                            autoright=chat_statistics["autoright"]
-                        )
 
-                        # TODO: message to chat about your creation
+                        if not chat_statistics:
+                            answer = "Broken data\n"
+                        else:
+                            my_user.create_chat(
+                                name=chat_statistics["name"],
+                                limit=chat_statistics["limit"],
+                                security_mode=chat_statistics["security_mode"],
+                                password=chat_statistics["password"],
+                                autoright=chat_statistics["autoright"]
+                            )
 
-                        answer = '{0: ^30}'.format('You successfully create new chat')
+                            # TODO: message to chat about your creation
+
+                            answer = '{0: ^30}\n'.format(
+                                f'You successfully create new chat {chat_statistics["name"]}'
+                            )
 
                     case 'add_to_chat':
                         pass
                     case 'quit_chat':
-                        pass
+                        my_user.quit_chat(args[0])
+
+                        # TODO: message to chat about your leave
+
+                        answer = '{0: ^30}\n'.format(f'You leave chat {args[0]}')
+
                     case 'delete_chat':
-                        pass
+                        chat: Chat = TM_chats[args[0]]
+                        access = chat.check_Rights(my_user, Rights.ADMINISTRATOR)
+
+                        if isinstance(access, Exception):
+                            answer = access.__str__()
+                        else:
+                            my_user.delete_chat(chat.name)
+
+                            # TODO: message to chat about action (for online people)
+
+                            answer = '{0: ^30}\n'.format(f'You delete chat {args[0]}')
+
+                    case 'info_chat':
+                        chat: Chat = TM_chats[name]
+
+                        response = my_user.info_chat(chat.name)
+
+                        answer = []
+                        answer.append(f'Name: {response["Name"]}')
+                        answer.append(f'Creator: {response["Creator"]}')
+                        answer.append('Participants:')
+                        for item in response['Participants']:
+                            answer.append(f'  - {item[0]}({item[1]})' + ('\tonline' if item[0] in response['Online'] else ''))
+
+                        answer = '\n'.join(answer)
+
                     case _:
                         continue
 
